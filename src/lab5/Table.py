2857037c3b612a831bd3ea49809684b059007b57
Danylo Cherniavskyi <danylo.chernyavskiy@gmail.com>
2023-03-08 02:03:13 +0200

    Highlight swapped rows

--- src/lab5/Table.py
+++ src/lab5/Table.py
@@ -16,6 +16,7 @@ class Table:
         self.__table.insert('', 0, text='', values=tuple(array))
 
     def show_matrix(self, matrix: np.ndarray):
+        self.unhighlight()
         self.__resize_table(len(matrix.max(0)))
 
         for (idx, el) in enumerate(matrix.max(0)):
@@ -24,7 +25,27 @@ class Table:
 
         while len(self.__table.get_children()) > 0:
             self.__table.delete(self.__table.get_children()[0])
-        np.apply_along_axis(lambda row: self.__table.insert('', 'end', text='', values=tuple(row)), axis=1, arr=matrix)
+        np.apply_along_axis(lambda row: self.__table.insert('', 'end', text='', values=tuple(row), tags=(self.__determine_tag(matrix, row))), axis=1, arr=matrix)
+
+    def __determine_tag(self, matrix: np.ndarray, row: np.ndarray) -> str:
+        row_max = matrix[matrix.max(1).argmax()]
+        row_min = matrix[matrix.min(1).argmin()]
+
+        if np.array_equal(row, row_max):
+            return 'max_row'
+
+        if np.array_equal(row, row_min):
+            return 'min_row'
+
+        return ''
+
+    def highlight_rows(self):
+        self.__table.tag_configure('max_row', background='magenta')
+        self.__table.tag_configure('min_row', background='red')
+
+    def unhighlight(self):
+        self.__table.tag_configure('max_row', background='white')
+        self.__table.tag_configure('min_row', background='white')
 
     def __init__(self, master):
         table = ttk.Treeview(master)
